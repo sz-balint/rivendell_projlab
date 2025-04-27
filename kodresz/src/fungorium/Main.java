09f3a738f78bb54e3efb840296b2c49e45e0a8bf
Szabo Balint Akos <akos.balint.szabo@gmail.com>
2025-04-27 12:04:03 +0200

    mentes.txt hozzáadása a projekthez

--- kodresz/src/fungorium/Main.java
+++ kodresz/src/fungorium/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	static Scanner scanner = new Scanner(System.in); // Scanner letrehozasa
     public static void main(String[] args) {
         System.out.println("Kerlek valassz egy tesztesetet:");
+        System.out.println(System.getProperty("user.dir"));
+
         
     }    
 }
